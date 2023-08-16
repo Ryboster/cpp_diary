2860faecc98315707cba3f254e0ecdc8110690fe
Ryboster <kakashise@o2.pl>
2023-08-16 17:55:52 +0100

    enumerations update

--- day4-u-defined-types-and-structures/enumerations.cpp
+++ day4-u-defined-types-and-structures/enumerations.cpp
@@ -1,37 +1,45 @@
 #include<iostream>
+#include<thread>
+#include<chrono>
 using namespace std;
 
-enum class Colour { red, blue, green };
-enum class Traffic_light { green, yellow, red };
-Colour col = Colour::red;
-Traffic_light light = Traffic_light::red;
+enum class Colour { red, blue, green }; // enumeration class
+enum class Traffic_light { green, yellow, red, rednyellow};
 
-int pick_colour(){
-    int choice = 0;
-    Colour myColour;
-    while (choice!=1 || choice !=2 || choice!=3){
-        cin>>choice;
-        switch(choice){
-            case 1: 
-                myColour = Colour::red;
-                return choice;
-            case 2:
-                myColour = Colour::blue;
-                return choice;
-            case 3:
-                myColour = Colour::green;
-                return choice;
-        }
+Colour col = Colour::red; // to access properties of enumeration type, use :: operator
+Traffic_light light = Traffic_light::green;
+
+
+Traffic_light operator++(Traffic_light& t){ // simulation of integer increments in enumeration type.
+/* This class takes an address to an initialized Traffic_light type object and changes it*/
+    switch (t){
+        case Traffic_light::green: return t=Traffic_light::yellow; //if light is green, change it to yellow
+        case Traffic_light::yellow: return t=Traffic_light::red; //if light is yellow, change it to red
+        case Traffic_light::red: return t=Traffic_light::rednyellow; //if light is red, change it to red and yellow
+        case Traffic_light::rednyellow: return t=Traffic_light::green; // if light is red and yellow, change it to green
     }
 }
 
 int main(){
-    int x = pick_colour();
-    cout<<x<<"\n";
-    switch (x){
-        case 1: cout<<"You chose red\n"; break;
-        case 2: cout<<"You chose blue\n"; break;
-        case 3: cout<<"You chose green\n"; break;
+    Traffic_light x = operator++(light);
+    /* Simulation of traffic lights - they switch from one position to the next until it's green */
+    while (x!=Traffic_light::green){
+        if(x==Traffic_light::red){
+            cout<<"Red: Halt!\n";
+            x = operator++(light); // "increment" lights
+            this_thread::sleep_for(std::chrono::seconds(1)); //sleep for 1 second
+        }
+        if(x==Traffic_light::rednyellow){
+            cout<<"Red & yellow: Get ready!\n";
+            x = operator++(light);  // "increment" lights
+            this_thread::sleep_for(std::chrono::seconds(1)); //sleep for 1 second
+        }
+        if (x==Traffic_light::yellow){
+            cout<<"Yellow: Slow down and stop!\n";
+            x = operator++(light);  // "increment" lights
+            this_thread::sleep_for(std::chrono::seconds(1)); //sleep for 1 second
+        }
     }
+    cout<<"Green: Go!\n";
 }
 
